c54424860b20511ade4571bd33b22d4039bb358f
Kostas Thomson <kostasthomson12@gmail.com>
2025-06-20 22:21:05 +0300

    update

--- netlify/functions/main.js
+++ netlify/functions/main.js
@@ -1,31 +1,50 @@
 exports.handler = async (event, context) => {
-	const allowedOrigin = [
+	const allowedOrigins = [
 		"https://kostasthomson.github.io",
-		"https://kthomasiadis.gr/",
-		"http://kthomasiadis.gr/",
+		"https://kthomasiadis.gr",
+		"http://kthomasiadis.gr",
 	];
-	let allowFlag = true;
-	const origin = event.headers.origin || event.headers.referer || "";
-	for (const allowed of allowedOrigin) {
-		allowFlag = allowFlag || !origin.startsWith(allowed);
-	}
-	if (!allowFlag) {
+
+	const origin = event.headers.origin || "";
+
+	// Validate origin
+	const isAllowedOrigin = allowedOrigins.some((allowed) =>
+		origin.startsWith(allowed)
+	);
+
+	if (!isAllowedOrigin) {
 		return {
 			statusCode: 403,
 			body: "Forbidden: Invalid origin " + origin,
 		};
 	}
+
+	// Handle preflight request
 	if (event.httpMethod === "OPTIONS") {
 		return {
 			statusCode: 200,
 			headers: {
 				"Access-Control-Allow-Origin": origin,
 				"Access-Control-Allow-Headers": "Content-Type",
-				"Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+				"Access-Control-Allow-Methods": "POST, OPTIONS",
 			},
 			body: "",
 		};
 	}
+
+	// Reject other methods
+	if (event.httpMethod !== "POST") {
+		return {
+			statusCode: 405,
+			headers: {
+				Allow: "POST, OPTIONS",
+				"Access-Control-Allow-Origin": origin,
+			},
+			body: "Method Not Allowed",
+		};
+	}
+
+	// Return Firebase config
 	return {
 		statusCode: 200,
 		headers: {
@@ -40,8 +59,6 @@ exports.handler = async (event, context) => {
 			FIREBASE_MessagingSenderId: process.env.FIREBASE_MessagingSenderId,
 			FIREBASE_AppId: process.env.FIREBASE_AppId,
 			FIREBASE_MeasurementId: process.env.FIREBASE_MeasurementId,
-			event,
-			context,
 		}),
 	};
 };
