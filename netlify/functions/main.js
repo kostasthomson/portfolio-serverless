12e95e0deb56ed5691e4e4d3ae1ef51a16d9195e
Kostas Thomson <kostasthomson12@gmail.com>
2025-06-20 22:12:19 +0300

    update

--- netlify/functions/main.js
+++ netlify/functions/main.js
@@ -13,6 +13,17 @@ exports.handler = async (event, context) => {
 			};
 		}
 	}
+	if (event.httpMethod === "OPTIONS") {
+		return {
+			statusCode: 200,
+			headers: {
+				"Access-Control-Allow-Origin": origin,
+				"Access-Control-Allow-Headers": "Content-Type",
+				"Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+			},
+			body: "",
+		};
+	}
 	return {
 		statusCode: 200,
 		headers: {
